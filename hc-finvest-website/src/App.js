1c37ab2143909b098b62a170e22994b4fbdc0ad1
Diptesh-Dhangar <dhangardiptesh2001@gmail.com>
2025-12-09 10:14:56 +0530

    App Css Alingement

--- hc-finvest-website/src/App.js
+++ hc-finvest-website/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-// import "./App.css";
+import "./App.css";
 import Navbar from "./Components/Navbar";
 import MainContent from "./Components/MainContent";
 import { Route, Routes } from "react-router-dom";
